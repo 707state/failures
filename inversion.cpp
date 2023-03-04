470329da078686c3ce8f2a1773a5bc683c2d23a9
宋冠颉 <115874695+707state@users.noreply.github.com>
2023-03-04 11:09:04 +0800

    Update inversion.cpp

--- inversion.cpp
+++ inversion.cpp
@@ -2,19 +2,19 @@
 #include<iostream>
 using namespace std;
 int inversion(int* head,int i,int n){
-
     int total=0;
+    if(i!=n-1){
     for(int j=i;j<n;j++){
         if(head[i]>head[j]){
             total++;
         }
     }
-    return total+inversion(head,i+1,n);
+    return total+inversion(head,i+1,n);}
+    else{
+        return total;
+    }
 }
 int main(){
-    printf("这是一个简单的逆序数求解器");
-    printf("第一行输入一个数，表示序列长度；第二行输入每个序数，以空格隔开");
-    printf("点击回车查看答案");
     int* sub=new int;
     int n;
     cin>>n;
